a04678598ff24b610876238be35af07a66b18f61
Jean-Michel Batty <jean-michel.batty@extend.com>
2023-03-21 10:51:17 -0500

    Original code commit
    Refer to Readme.md for instructions on how to install

---
+++ SCG/Extend/view/frontend/requirejs-config.js
@@ -0,0 +1,10 @@
+var config = {
+    map: {
+        '*': {
+            extendWarrantyOffers: 'SCG_Extend/js/warranty-offers-base',
+            simpleProductWarranty: 'SCG_Extend/js/product/simple-warranty',
+            configurableProductWarranty: 'SCG_Extend/js/product/configurable-warranty',
+        }
+    },
+
+};
